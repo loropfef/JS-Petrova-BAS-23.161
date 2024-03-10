9225a68f680a6c939346d737d2014118650114e9
loropfef <156899586+loropfef@users.noreply.github.com>
2024-03-10 09:28:49 +0300

    Update first-program9.js

--- Практика Javascript 001/first-program9.js
+++ Практика Javascript 001/first-program9.js
@@ -1,5 +1,6 @@
 let age = 17;
 let pulseAtRest = 60;
 let intensity = 75;
+let pulseAtWorkout;
 pulseAtWorkout = ((220 - age) - (pulseAtRest)) * (intensity +  pulseAtRest);
 Math.round(pulseAtWorkout);

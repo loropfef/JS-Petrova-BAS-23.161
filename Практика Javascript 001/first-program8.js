7e6147e5ae7a19eaabd8147410fb813fd2e59ef7
loropfef <156899586+loropfef@users.noreply.github.com>
2024-03-10 09:28:26 +0300

    Update first-program8.js

--- Практика Javascript 001/first-program8.js
+++ Практика Javascript 001/first-program8.js
@@ -1,6 +1,8 @@
 let firstLeg = 300;
 let secondLeg = 700;
+let hypotenuse;
 hypotenuse = (firstLeg * firstLeg) + (secondLeg * secondLeg);
 Math.sqrt(hypotenuse);
 Math.round(Math.sqrt(hypotenuse));
+let perimeter;
 perimeter = firstLeg + secondLeg + Math.round(Math.sqrt(hypotenuse));

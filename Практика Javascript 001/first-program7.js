76ba135598b458caca220d96b135a466d8d1196e
loropfef <156899586+loropfef@users.noreply.github.com>
2024-03-10 09:27:27 +0300

    Update first-program7.js

--- Практика Javascript 001/first-program7.js
+++ Практика Javascript 001/first-program7.js
@@ -1,7 +1,9 @@
 let length = 1.83;
 let weight = 75;
 let fatMass = 0.8;
+let bodyMassIndex;
 bodyMassIndex = weight / (length * length);
 Math.round(bodyMassIndex);
+let fatPercent;
 fatPercent = (fatMass / weight) * 100;
 Math.round(fatPercent);

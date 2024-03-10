18549f474439d9e8b12cab1a9c67b9fffe167f1e
loropfef <156899586+loropfef@users.noreply.github.com>
2024-03-10 09:14:00 +0300

    Update first-program.js

--- Практика Javascript 001/first-program.js
+++ Практика Javascript 001/first-program.js
@@ -1,6 +1,6 @@
-timeInHours;
+let timeInHours;
 timeInHours = prompt("сколько часов ты провел в колледже?");
 'Данные получены. Проведено ' + timeInHours + ' часов.'
-timeInMinutes;
+let timeInMinutes;
 timeInMinutes = timeInHours * 60;
 'На учебе проведено ' + timeInMinutes + ' минут!'
